b863486979c5b61416f84ca952b97bf380dc99f9
clamxyz <zhwq2012@gmail.com>
2014-02-11 21:21:47 +0800

    new file:   timer/Makefile 	modified:   timer/timer.c 	modified:   ../ch1/.vimrc

--- ch10/timer/timer.c
+++ ch10/timer/timer.c
@@ -7,7 +7,8 @@
 #include <limits.h>
 
 #ifdef BOOL
-#undef
+#undef BOOL
+#endif
 #define BOOL char 
 #define TRUE 1
 #define FALSE 0 
@@ -36,10 +37,19 @@ extern int errno;
 
 static tmr_instance_t g_tmr_instance[MAXTMRS];
 static sig_atomic_t g_hasinit = HAS_NOT_INIT;
-static int g_min_interval = MAX_UINT;
+static long g_min_interval = UINT_MAX;
 
 static void interrupt_handler(int signo);
 static int tmr_init(void);
+static void start_physic_timer(long usec);
+static int valid_tid(int tid);
+static long get_secs_from_itimerval(struct itimerval itval);
+static void set_secs_to_itimerval(struct itimerval *pitval, int usec)
+
+static void interrupt_handler(int signo)
+{
+	
+}
 
 static int tmr_init(void)
 {
@@ -69,6 +79,40 @@ static int tmr_init(void)
 	return 0;	
 }
 
+static void start_physic_timer(long usec)
+{
+	struct itimerval itval;
+	itval.it_interval.tv_sec = 0;
+	itval.it_interval.tv_usec = 0;
+	itval.it_value.tv_sec = usec / 1000;
+	itval.it_value.tv_usec = usec % 1000;
+	setitimer(ITIMER_REAL, &itval, NULL);	 
+}
+
+static int valid_tid(int tid)
+{
+	if (tid < 0 || tid >= MAXTMRS)
+	{
+		errno = ERANGE;
+		return -1;
+	}
+
+	return 0;
+}
+
+static long get_secs_from_itimerval(struct itimerval itval)
+{
+	return itval.it_value.tv_sec * 1000 + itval.it_val.tv_usec;
+}	
+
+static void set_secs_to_itimerval(struct itimerval *pitval, int usec)
+{
+	pitval->it_value.tv_sec = usec / 1000;
+	pitval->it_value.tv_usec = usec % 1000;
+	pitval->it_interval.tv_sec = 0;
+	pitval->it_interval.tv_usec = 0; 
+}
+
 int timer_create(int usec, void (*handler)(int))
 {
 	int i = 0;
@@ -78,7 +122,10 @@ int timer_create(int usec, void (*handler)(int))
 		errno = EINVAL;
 		return -1;
 	}
-	
+	if (!g_hasinit && tmr_init() != 0)
+	{
+		return -1;
+	}	
 	while(i < MAXTMRS && g_tmr_instance[i].tmr_status == NO_IN_USE)
 	{
 		g_tmr_instance[i].tmr_status = STOP;
@@ -97,15 +144,74 @@ int timer_create(int usec, void (*handler)(int))
 
 int timer_start(int tid)
 {
-			
+	sigset_t newmask, oldmask;
+	long usec;
+	struct itimerval cur_itval;
+	struct itimerval next_itval;
+	int i = 0;
+
+	if (valid_tid(tid) == -1)
+		return -1;
+	if (g_tmr_instance[tid].tmr_status != STOP)
+	{
+		errno = EINVAL;
+		return -1;
+	}	
+	sigemptyset(&newmask);
+	sigaddset(&newmask, SIGALRM);
+	sigprocmask(SIG_BLOCK, &newmask, &oldmask);
+	getitimerval(ITIMER_REAL, &cur_itval);
+	usec = get_secs_from_itimerval(cur_itval);
+	if (usec == 0)
+	{
+		set_secs_to_itimerval(&cur_itval, g_tmr_instance[tid].tmr_interval);
+		setitimerval(ITIMER_REAL, &cur_itval, NULL);
+	}
+	else
+	{
+		
+	}
+	sigprocmask(SIG_SETMASK, &oldmask, NULL);
+		
 }	
 
 int timer_stop(int tid)
 {
-	g_tmr_instance[tid].tmr_status = STOP;	
+	int i = 0;
+	if (vaild_tid(tid) == -1)
+		return -1;
+	if (g_tmr_instance[tid].tmr_status == NOT_IN_USE)
+	{
+		errno = EINVAL;
+		return -1;
+	}
+	
+	g_tmr_instance[tid].tmr_status = STOP;
+	
+	while(i < MAXTMRS && g_tmr_instance[i++].tmr_status != USING )
+			;
+	if (i == MAXTMRS)
+	{
+		g_min_interval = UINT_MAX;
+	}
 }
 
 int timer_close(int tid)
 {
+	int i = 0;
+	if (valid_tid(tid) == -1)
+		return -1;
+	if (g_tmr_instance[tid].tmr_status == NOT_IN_USE)
+	{
+		errno = EINVAL;
+		return -1;
+	}
 	g_tmr_instance[tid].tmr_status = NOT_IN_USE;
+	while (i < MAXTMRS && g_tmr_instance[i++].tmr_status != USING)
+		;
+	if (i == MAXTMRS)
+	{
+		g_min_interval = UINT_MAX;
+	}	
 }
+
